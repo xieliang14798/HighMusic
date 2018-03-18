5a0f2606a75df7741dd7681544fa9e17fc519562
xieliang <1186654762@qq.com>
2018-03-18 23:57:30 +0800

    play vue

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,55 +1,228 @@
 <template>
   <div class="player">
-    <div class="normal-player" v-show="fullScreen">
-      <div class="background">
-        <img :src="currentSong.image">
-      </div>
-      <div class="top">
-        <div class="back" @click="back">
-          <i class="icon-back"></i>
+    <transition name="normal"
+                @enter="enter"
+                @after-enter="afterEnter"
+                @leave="leave"
+                @after-leave="afterLeave">
+      <div class="normal-player" v-show="fullScreen">
+        <div class="background">
+          <img :src="currentSong.image">
         </div>
-        <h1 class="title" v-html="currentSong.name"></h1>
-        <h2 class="subtitle" v-html="currentSong.singer"></h2>
+        <div class="top">
+          <div class="back" @click="back">
+            <i class="icon-back"></i>
+          </div>
+          <h1 class="title" v-html="currentSong.name"></h1>
+          <h2 class="subtitle" v-html="currentSong.singer"></h2>
+        </div>
+        <div class="middle">
+          <div class="middle-l" ref="middleL">
+            <div class="cd-wrapper" ref="cdWrapper">
+              <div class="cd" :class="cdCls">
+                <img class="image" :src="currentSong.image">
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="bottom">
+          <div class="dot-wrapper">
+            <span class="dot active"></span>
+            <span class="dot"></span>
+          </div>
+          <div class="operators">
+            <div class="icon i-left">
+              <i class="icon-loop"></i>
+            </div>
+            <div class="icon i-left" :class="disableCls">
+              <i class="icon-prev" @click="prev"></i>
+            </div>
+            <div class="icon i-center" :class="disableCls">
+              <i :class="playIcon" @click="togglePlaying"></i>
+            </div>
+            <div class="icon i-right" :class="disableCls">
+              <i class="icon-next" @click="next"></i>
+            </div>
+            <div class="icon i-right">
+              <i class="icon-favorite"></i>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-    <div class="mini-player" v-show="!fullScreen" @click="open">
-      <div class="icon">
-        <img :src="currentSong.image" :class="cdCls">
+    </transition>
+    <transition name="mini">
+      <div class="mini-player" v-show="!fullScreen" @click="open">
+        <div class="icon">
+          <img :src="currentSong.image" :class="cdCls">
+        </div>
+        <div class="text">
+          <h2 class="name" v-html="currentSong.name"></h2>
+          <p class="desc" v-html="currentSong.singer"></p>
+        </div>
+        <div class="control">
+          <i class="icon-mini" :class="miniIcon" @click.stop="togglePlaying"></i>
+        </div>
       </div>
-    </div>
+    </transition>
+    <audio :src="currentSong.url" ref="audio" @play="ready" @error="error"></audio>
   </div>
 </template>
 <script>
   import {mapGetters, mapMutations} from 'vuex'
+  import animations from 'create-keyframe-animation'
+  import {prefixStyle} from "common/js/dom";
+
+  const transform = prefixStyle('transform')
 
   export default {
     data() {
-      return {}
+      return {
+        songReady: false
+      }
     },
     mounted() {
-      console.log(2)
-      console.log(this.currentSong)
     },
     computed: {
       ...mapGetters([
         'playing',
         'fullScreen',
         'currentIndex',
-        'currentSong'
+        'currentSong',
+        'playlist'
       ]),
       cdCls() {
         return this.playing ? 'play' : 'play pause'
+      },
+      playIcon() {
+        return this.playing ? 'icon-pause' : 'icon-play'
+      },
+      miniIcon() {
+        return this.playing ? 'icon-pause-mini' : 'icon-play-mini'
+      },
+      disableCls() {
+        return this.songReady ? '' : 'disable'
+      }
+    },
+    watch: {
+      playing(newPlaying) {
+        const audio = this.$refs.audio
+        this.$nextTick(() => {
+          newPlaying ? audio.play() : audio.pause()
+        })
+      },
+      currentSong(newSong, oldSong) {
+        if (!newSong.id) {
+          return
+        }
+        if (newSong.id === oldSong.id) {
+          return
+        }
+        this.$nextTick(() => {
+          this.$refs.audio.play()
+        })
       }
     },
     methods: {
       ...mapMutations({
-        setFullScreen: 'SET_FULL_SCREEN'
+        setFullScreen: 'SET_FULL_SCREEN',
+        setPlayingState: 'SET_PLAYING_STATE',
+        setCurrentIndex: 'SET_CURRENT_INDEX'
       }),
       back() {
         this.setFullScreen(false)
       },
       open() {
         this.setFullScreen(true)
+      },
+      togglePlaying() {
+        this.setPlayingState(!this.playing)
+      },
+      ready() {
+        this.songReady = true
+      },
+      error() {
+        this.songReady = true
+      },
+      prev() {
+        if (!this.songReady) {
+          return
+        }
+        let index = this.currentIndex - 1;
+        if (index === -1) {
+          index = this.playlist.length - 1
+        }
+        this.setCurrentIndex(index)
+        if (!this.playing) {
+          this.togglePlaying()
+        }
+        this.songReady = false
+      },
+      next() {
+        if (!this.songReady) {
+          return
+        }
+        let index = this.currentIndex + 1;
+        if (index === this.playlist.length) {
+          index = 0
+        }
+        this.setCurrentIndex(index)
+        if (!this.playing) {
+          this.togglePlaying()
+        }
+        this.songReady = false
+      },
+      _getPosAndScale() {
+        const targetWidth = 40
+        const paddingLeft = 40
+        const paddingBottom = 30
+        const paddingTop = 80
+        const width = window.innerWidth * 0.8
+        const scale = targetWidth / width
+        const x = -(window.innerWidth / 2 - paddingLeft)
+        const y = window.innerHeight - paddingTop - width / 2 - paddingBottom
+        return {
+          x,
+          y,
+          scale
+        }
+      },
+      enter(el, done) {
+        const {x, y, scale} = this._getPosAndScale()
+
+        let animation = {
+          0: {
+            transform: `translate3d(${x}px,${y}px,0) scale(${scale})`
+          },
+          60: {
+            transform: `translate3d(0,0,0) scale(1.1)`
+          },
+          100: {
+            transform: `translate3d(0,0,0) scale(1)`
+          }
+        }
+        animations.registerAnimation({
+          name: 'move',
+          animation,
+          presets: {
+            duration: 400,
+            easing: 'linear'
+          }
+        })
+        animations.runAnimation(this.$refs.cdWrapper, 'move', done)
+      },
+      afterEnter() {
+        animations.unregisterAnimation('move')
+        this.$refs.cdWrapper.style.animation = ''
+      },
+      leave(el, done) {
+        this.$refs.cdWrapper.style.transition = 'all 0.4s'
+        const {x, y, scale} = this._getPosAndScale()
+        this.$refs.cdWrapper.style[transform] = `translate3d(${x}px,${y}px,0) scale(${scale})`
+        this.$refs.cdWrapper.addEventListener('transitionend', done)
+      },
+      afterLeave() {
+        this.$refs.cdWrapper.style.transition = ''
+        this.$refs.cdWrapper.style[transform] = ''
       }
     }
   }
@@ -76,6 +249,21 @@
       bottom: 0;
       z-index: 150;
       background-color: @color-background;
+      &.normal-enter-active, &.normal-leave-active {
+        transition: all 0.4s;
+        .top, .bottom {
+          transition: all 0.4s cubic-bezier(0.86, 0.18, 0.82, 1.32)
+        }
+      }
+      &.normal-enter, &.normal-leave-to {
+        opacity: 0;
+        .top {
+          transform: translate3d(0, -100px, 0)
+        }
+        .bottom {
+          transform: translate3d(0, 100px, 0)
+        }
+      }
       .background {
         position: absolute;
         left: 0;
@@ -123,6 +311,104 @@
           color: @color-text;
         }
       }
+      .middle {
+        position: fixed;
+        width: 100%;
+        top: 80px;
+        bottom: 170px;
+        white-space: nowrap;
+        font-size: 0;
+        .middle-l {
+          display: inline-block;
+          vertical-align: top;
+          position: relative;
+          width: 100%;
+          height: 0;
+          padding-top: 80%;
+          .cd-wrapper {
+            position: absolute;
+            left: 10%;
+            top: 0;
+            width: 80%;
+            height: 100%;
+            .cd {
+              width: 100%;
+              height: 100%;
+              box-sizing: border-box;
+              border: 10px solid rgba(255, 255, 255, 0.1);
+              border-radius: 50%;
+              &.play {
+                animation: rotate 20s linear infinite;
+              }
+              &.pause {
+                animation-play-state: paused;
+              }
+              .image {
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                border-radius: 50%;
+              }
+            }
+          }
+        }
+      }
+      .bottom {
+        position: absolute;
+        bottom: 50px;
+        width: 100%;
+        .dot-wrapper {
+          text-align: center;
+          font-size: 0;
+          .dot {
+            display: inline-block;
+            vertical-align: center;
+            margin: 0 4px;
+            width: 8px;
+            height: 8px;
+            border-radius: 50%;
+            background: @color-text-l;
+            &.active {
+              width: 20px;
+              border-radius: 5px;
+              background: @color-text-ll;
+            }
+          }
+        }
+        .operators {
+          display: flex;
+          align-items: center;
+          .icon {
+            flex: 1;
+            color: @color-theme;
+            &.disable {
+              color: @color-theme-d;
+            }
+            i {
+              font-size: 30px;
+            }
+          }
+          .i-left {
+            text-align: right;
+          }
+          .i-center {
+            padding: 0 20px;
+            text-align: center;
+            i {
+              font-size: 40px;
+            }
+          }
+          .i-right {
+            text-align: left;
+          }
+          .icon-favorite {
+            color: @color-sub-theme;
+          }
+
+        }
+      }
     }
     .mini-player {
       position: fixed;
@@ -134,6 +420,12 @@
       width: 100%;
       height: 60px;
       background-color: @color-highlight-background;
+      &.mini-enter-active, &.mini-leave-active {
+        transition: all 0.4s;
+      }
+      &.mini-enter, &.mini-leave-to {
+        opacity: 0;
+      }
       .icon {
         flex: 0 0 40px;
         width: 40px;
@@ -150,6 +442,42 @@
           }
         }
       }
+      .text {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        flex: 1;
+        line-height: 20px;
+        overflow: hidden;
+        .name {
+          margin-bottom: 2px;
+          .no-wrap();
+          font-size: @font-size-medium;
+          color: @color-text;
+        }
+        .desc {
+          .no-wrap();
+          font-size: @font-size-small;
+          color: @color-text-d;
+        }
+      }
+      .control {
+        flex: 0 0 30px;
+        width: 30px;
+        padding: 0 10px;
+        position: relative;
+        .icon-play-mini, .icon-pause-mini, .icon-playlist {
+          font-size: 30px;
+          color: @color-theme-d;
+        }
+        .icon-mini {
+          font-size: 32px;
+          position: absolute;
+          left: 0;
+          top: 0;
+        }
+      }
+
     }
   }
 </style>
